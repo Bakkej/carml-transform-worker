687e4cc7329fa03c5c2e375d457ef28142b39a3a
Jesse Bakker <jesse.bakker@kadaster.nl>
2023-05-10 22:01:32 +0200

    cleanup

--- run.bat
+++ run.bat
@@ -1,7 +1,20 @@
-REM not working under Windows
+@REM REM not working under Windows
 
-@REM curl -L https://github.com/carml/carml-jar/releases/download/v1.2.0/carml-jar-rdf4j-1.2.0-0.4.7.jar -o carml-rdf4j.jar
+@REM @REM curl -L https://github.com/carml/carml-jar/releases/download/v1.2.0/carml-jar-rdf4j-1.2.0-0.4.7.jar -o carml-rdf4j.jar
 
-REM  Run
+@REM REM  Run
 
-java -jar carml-rdf4j.jar map -m rml -rsl input -of ttl -P -o output
+@REM 
+
+@REM cat input/* | java -jar carml-rdf4j.jar map -m rml/mapping.rml.ttl  -of ttl -P -o output/ -p imx-geo,rdfs,best -pm rml/prefix.json
+
+
+
+@REM cat input | java -jar carml-rdf4j.jar map -m rml/mapping.rml.ttl -of ttl -P -o output
+
+
+@REM netage
+
+@REM curl -L https://github.com/netage/carml-cli/releases/download/cli-1.1.1/cli-1.1.1-SNAPSHOT-jar-with-dependencies.jar -o netage.jar
+
+java -jar netage.jar -f input -m rml/mapping.rml.ttl -o output.ttl -of ttl
