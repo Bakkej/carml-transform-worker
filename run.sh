7f0cc2e029cc515b3a6a3d36dcd0498b3589ff60
Jesse Bakker <jesse.bakker@kadaster.nl>
2023-05-10 21:42:28 +0200

    loop shell

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 curl -L https://github.com/carml/carml-jar/releases/download/v1.2.0/carml-jar-rdf4j-1.2.0-0.4.7.jar -o carml-rdf4j.jar
 
+_dfiles="/input/*"
 
-java -jar carml-rdf4j.jar map -m rml -rsl input -of ttl -P -o output
+for f in _dfiles
+do
+ echo "Processing $f" # always double quote "$f" filename
+ # do something on $f
+ cat input/$f | java -jar carml-rdf4j.jar map -m rml/mapping.rml.ttl  -of ttl -P -o output/$f.ttl -p imx-geo,rdfs,best -pm rml/prefix.json 
+done
+
