6d51f4411e559736e26fad4b2cca100c09ba6d4c
Jesse Bakker <jesse.bakker@kadaster.nl>
2023-05-11 15:46:04 +0200

    add one

--- run.bat
+++ run.bat
@@ -1,12 +1,14 @@
 @REM REM not working under Windows
 
 @REM @REM curl -L https://github.com/carml/carml-jar/releases/download/v1.2.0/carml-jar-rdf4j-1.2.0-0.4.7.jar -o carml-rdf4j.jar
-
-@REM REM  Run
-
+ 
+@REM REM  Run 
+ 
 @REM 
-
-@REM cat input/* | java -jar carml-rdf4j.jar map -m rml/mapping.rml.ttl  -of ttl -P -o output/ -p imx-geo,rdfs,best -pm rml/prefix.json
+ 
+cat input/zeewolde.Dubbelbestemming.gml | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl -of ttl -o output/zeewolde.Dubbelbestemming.ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json
+cat input/zeewolde.Enkelbestemming_1.gml | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl -of ttl -o output/zeewolde.Enkelbestemming_1.ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json
+cat input/zeewolde.Enkelbestemming_2.gml | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl -of ttl -o output/zeewolde.Enkelbestemming_2.ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json
 
 
 
@@ -17,4 +19,12 @@
 
 @REM curl -L https://github.com/netage/carml-cli/releases/download/cli-1.1.1/cli-1.1.1-SNAPSHOT-jar-with-dependencies.jar -o netage.jar
 
-java -jar netage.jar -f input -m rml/mapping.rml.ttl -o output.ttl -of ttl
+@REM java -jar netage.jar -f input -m rml/mapping.rml.ttl -o output.ttl -of ttl
+
+@REM for f in input/*.gml
+@REM do
+@REM  echo "Processing $f" # always double quote "$f" filename
+@REM  # do something on $f
+@REM #  cat $f | java -jar carml-rdf4j.jar map -m rml/mapping.rml.ttl  -of ttl -P -o output/$(basename ${f} .gml).ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json 
+@REM  cat $f | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl  -of ttl -o output/$(basename ${f} .gml).ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json 
+@REM done
--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
-curl -L https://github.com/carml/carml-jar/releases/download/v1.2.0/carml-jar-rdf4j-1.2.0-0.4.7.jar -o carml-rdf4j.jar
+# curl -L https://github.com/carml/carml-jar/releases/download/v1.2.0/carml-jar-rdf4j-1.2.0-0.4.7.jar -o carml-rdf4j.jar
+
 
 for f in input/*.gml
 do
  echo "Processing $f" # always double quote "$f" filename
  # do something on $f
-   cat $f | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl -P -of ttl -o output/$(basename ${f} .gml).ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json 
+   cat $f | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl -of ttl -o output/$(basename ${f} .gml).ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json 
 #  cat $f | java -jar rml/carml-geo.jar map -m rml/mapping.rml.ttl  -of ttl -o output/$(basename ${f} .gml).ttl -p imx-geo,rdfs,best,nen3610 -pm rml/prefix.json
 done
 
